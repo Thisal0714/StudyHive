1021a4bb53e6dc874424861748f96045f514a10b
ThisalC <thisalchamodya123@gmail.com>
2025-06-28 10:22:54 +0530

    Initial commit

---
+++ src/main/java/StudyHive/ThisalCL/ThisalClApplication.java
@@ -0,0 +1,13 @@
+package StudyHive.ThisalCL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThisalClApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThisalClApplication.class, args);
+	}
+
+}
