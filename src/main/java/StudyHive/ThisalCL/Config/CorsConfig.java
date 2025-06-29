4309615253df7fc93418e9b058fe0b89af4dae32
ThisalC <thisalchamodya123@gmail.com>
2025-06-29 16:35:42 +0530

    Origin added

--- src/main/java/StudyHive/ThisalCL/Config/CorsConfig.java
+++ src/main/java/StudyHive/ThisalCL/Config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://studyhive-5z2u.onrender.com")
+                        .allowedOrigins("https://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
