44749767a83c21082da52d230728d72bba6145fd
ThisalC <thisalchamodya123@gmail.com>
2025-06-30 11:08:04 +0530

    Local dev CORS error fixed

--- src/main/java/StudyHive/ThisalCL/Service/UsersManagementService.java
+++ src/main/java/StudyHive/ThisalCL/Service/UsersManagementService.java
@@ -86,10 +86,6 @@ public class UsersManagementService {
         return response;
     }
 
-
-
-
-
     public ReqRes refreshToken(ReqRes refreshTokenReqiest){
         ReqRes response = new ReqRes();
         try{
