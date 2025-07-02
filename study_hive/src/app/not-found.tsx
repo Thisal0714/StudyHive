1906a74f435f4aed6fbbbb96ac86d21275b0859f
ThisalC <thisalchamodya123@gmail.com>
2025-07-02 18:42:50 +0530

    chore: admin role set and dashboard navigation set for admin, register form dropdowns restricted

--- study_hive/src/app/not-found.tsx
+++ study_hive/src/app/not-found.tsx
@@ -1,12 +1,17 @@
 import Link from "next/link";
 
 export default function CustomNotFound() {
-    return (
-      <div className="min-h-screen flex flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold mb-4">404 — Page Not Found</h1>
-        <p className="text-gray-600 mb-6">Looks like you’re lost in StudyHive 👻</p>
-        <Link href="/" className="text-blue-600 underline hover:text-blue-800">Back to Home</Link>
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-white">
+      <h1 className="text-4xl font-bold mb-4 text-black">404 — Page Not Found</h1>
+      <p className="text-gray-600 mb-6">
+        Looks like you’re lost!
+      </p>
+      <div className="flex items-center bg-primary text-white px-4 py-2 rounded-lg cursor-pointer text-lg">
+        <Link href="/dashboard">
+          Back to Home
+        </Link>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
