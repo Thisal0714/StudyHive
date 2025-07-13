279791254ae3044008e1e4e75a045c3d31e0b78f
ThisalC <thisalchamodya123@gmail.com>
2025-07-13 13:27:22 +0530

    feat: fullstop added

--- study_hive/src/app/not-found.tsx
+++ study_hive/src/app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function CustomNotFound() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-white">
-      <h1 className="text-4xl font-bold mb-4 text-black">404 — Page Not Found</h1>
+      <h1 className="text-4xl font-bold mb-4 text-black">404 — Page Not Found.</h1>
       <p className="text-gray-600 mb-6">
         Looks like you’re lost!
       </p>
